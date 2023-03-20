d3c40e33752cb869381cf1255313c0780b47d2d3
Tvilakazi <thabiv1119@gmail.com>
2023-03-20 14:52:41 -0700

    Function-like macro

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - prints the name of the file
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
-int ain(void)
+int main(int ac, char **av)
 {
-	printf("%s\n", __FILE__);
-	return (0);
+    int res;
+
+    if (ac != 3)
+    {
+        dprintf(2, "Usage: %s filename text\n", av[0]);
+        exit(1);
+    }
+    res = append_text_to_file(av[1], av[2]);
+    printf("-> %i)\n", res);
+    return (0);
 }
 
